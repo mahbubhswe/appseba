902a8f620e570a0c93a80ffa3e713aadb44d5fdc
mahbubhswe <mahbubhswe@gmail.com>
2024-12-11 11:00:25 +0600

    u

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <Layout
-      pageTitle="App Seba - Sotware Company.Mobile & Web Application Development"
+      pageTitle="App Seba - Sotware Company"
       pageDescription="App Seba is a Bangladeshi software development company specializing in web and mobile app development. We create innovative solutions using technologies like React.js, Node.js, Flutter, Firebase, and more."
       pageKeywords="app development, software development, web applications, mobile apps, Flutter, React.js, Node.js, Firebase, Bangladesh, cross-platform apps, app store services, custom software solutions"
     >
